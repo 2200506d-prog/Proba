67fbf53c47cea73d04bcd659f08d8cbd90347bcb
2200506d-prog <2200506d@umich.mx>
2026-02-17 12:00:28 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nombre = document.getElementById('nombre');
+    console.log('Página cargada. Mostrando: ' + nombre.textContent);
+});
